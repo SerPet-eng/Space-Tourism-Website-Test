06fa009ba7bdde01a9b8c58791117f5319cfdde6
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2024-12-12 21:56:06 +0800

    Update App.jsx
     - Removing the test <div className="component">
     This Component is for testing purposes only.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-import Button from './components/Button';
-import Pagination from './components/Pagination';
-import SmallPagination from './components/SmallPagination';
-import DestinationTab from './components/DestinationTab';
 
 import Home from './components/Home/Home';
-import Destination from './components/Destination/Destination';
-import Technology from './components/Technology/Technology';
-import Crew from './components/Crew/Crew';
+
 import NotFound from './components/Error/NotFound';
-import { usePageContext } from './utils/PageContext';
 
 import DestinationRoutes from './routes/DestinationRoutes';
 import CrewRoutes from './routes/CrewRoutes';
 import TechnologyRoutes from './routes/TechnologyRoutes';
 
 export default function App() {
-  const { destinations, crew, technology, loading } = usePageContext();
-
   return (
     <>
       <div className="app">
@@ -41,57 +32,6 @@ export default function App() {
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </div>
-
-      <div className="components" style={{ display: 'none' }}>
-        <NotFound />
-
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          destinations.length > 0 && (
-            <Destination
-              img={destinations[0].images.png}
-              name={destinations[0].name}
-              description={destinations[0].description}
-              distance={destinations[0].distance}
-              travel={destinations[0].travel}
-            />
-          )
-        )}
-
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          crew.length > 0 && (
-            <Crew
-              img={crew[0].images.png}
-              name={crew[0].name}
-              role={crew[0].role}
-              bio={crew[0].bio}
-            />
-          )
-        )}
-
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          technology.length > 0 && (
-            <Technology
-              img_portrait={technology[0].images.portrait}
-              img_landscape={technology[0].images.landscape}
-              name={technology[0].name}
-              description={technology[0].description}
-            />
-          )
-        )}
-
-        <Home />
-        <Navbar />
-        <Button />
-        <Pagination />
-        <SmallPagination />
-        <DestinationTab />
       </div>
     </>
   );
