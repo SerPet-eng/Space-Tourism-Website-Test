98ed103a3a0a46d117e952b16bf25886c762de4c
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2024-12-16 14:24:51 +0800

    Update vite.config.jsx

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-lazy-load-image-component'],
-    },
+  optimizeDeps: {
+    include: ['react-lazy-load-image-component'], // Ensure the library is pre-bundled
   },
 });
